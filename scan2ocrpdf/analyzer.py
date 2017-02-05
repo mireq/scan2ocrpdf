e738239812af3c5ca3aa29a654f30f2f94b06e6c
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-02-05 14:57:13 +0100

    Added function decode_block

--- scan2ocrpdf/analyzer.py
+++ scan2ocrpdf/analyzer.py
@@ -10,7 +10,6 @@ class Page(object):
 
 class Block(object):
 	bounding_box = None
-	block_type = None
 	image = None
 
 
@@ -27,27 +26,38 @@ class Analyzer(object):
 		self.api.SetImage(image)
 		self.api.Recognize()
 		iterator = self.api.GetIterator()
-		for tesseract_block in iterate_level(iterator, RIL.BLOCK):
-			block = Block()
-			block.bounding_box = tesseract_block.BoundingBox(RIL.BLOCK)
-			block.block_type = tesseract_block.BlockType()
-			if not block.block_type in self.TEXT_TYPES:
-				block.image = tesseract_block.GetImage(RIL.BLOCK, 0, image)
-			for tesseract_para in iterate_level(iterator, RIL.PARA):
-				for tesseract_textline in iterate_level(iterator, RIL.TEXTLINE):
-					for tesseract_word in iterate_level(iterator, RIL.WORD):
-						for symbol in iterate_level(iterator, RIL.SYMBOL):
-							print("symbol")
-							if iterator.IsAtFinalElement(RIL.WORD, RIL.SYMBOL):
-								break
-						if iterator.IsAtFinalElement(RIL.TEXTLINE, RIL.WORD):
-							break
-					if iterator.IsAtFinalElement(RIL.PARA, RIL.TEXTLINE):
-						break
-				if iterator.IsAtFinalElement(RIL.BLOCK, RIL.PARA):
-					break
+		page.blocks = self.__decode_blocks(iterator, image)
+		#for tesseract_block in iterate_level(iterator, RIL.BLOCK):
+		#	block = Block()
+		#	block.bounding_box = tesseract_block.BoundingBox(RIL.BLOCK)
+		#	block.block_type = tesseract_block.BlockType()
+		#	if not block.block_type in self.TEXT_TYPES:
+		#		block.image = tesseract_block.GetImage(RIL.BLOCK, 0, image)
+		#	for tesseract_para in iterate_level(iterator, RIL.PARA):
+		#		for tesseract_textline in iterate_level(iterator, RIL.TEXTLINE):
+		#			for tesseract_word in iterate_level(iterator, RIL.WORD):
+		#				for symbol in iterate_level(iterator, RIL.SYMBOL):
+		#					if iterator.IsAtFinalElement(RIL.WORD, RIL.SYMBOL):
+		#						break
+		#				if iterator.IsAtFinalElement(RIL.TEXTLINE, RIL.WORD):
+		#					break
+		#			if iterator.IsAtFinalElement(RIL.PARA, RIL.TEXTLINE):
+		#				break
+		#		if iterator.IsAtFinalElement(RIL.BLOCK, RIL.PARA):
+		#			break
 
 		return page
 
 	def close(self):
 		self.api.End()
+
+	def __decode_blocks(self, iterator, image):
+		blocks = []
+		for tesseract_block in iterate_level(iterator, RIL.BLOCK):
+			print(tesseract_block.GetUTF8Text(RIL.BLOCK))
+			block = Block()
+			block.bounding_box = tesseract_block.BoundingBox(RIL.BLOCK)
+			if not tesseract_block.BlockType() in self.TEXT_TYPES:
+				block.image = tesseract_block.GetImage(RIL.BLOCK, 0, image)
+			blocks.append(block)
+		return blocks
