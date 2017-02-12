b8e497563243ebd49f05ad27e7620882f22e2b69
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-02-12 10:03:50 +0100

    Added HTML output

--- scan2ocrpdf/analyzer.py
+++ scan2ocrpdf/analyzer.py
@@ -4,8 +4,51 @@ from __future__ import unicode_literals
 from tesserocr import PyTessBaseAPI, PSM, PT, RIL, iterate_level
 
 
+class Size(object):
+	width = 0
+	height = 0
+
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+
+
+class BoundingBox(object):
+	x1 = 0
+	y1 = 0
+	width = 0
+	height = 0
+
+	@property
+	def x2(self):
+		return self.x1 + self.width
+
+	@property
+	def y2(self):
+		return self.y1 + self.height
+
+	@staticmethod
+	def from_size(x1, y1, width, height):
+		instance = BoundingBox()
+		instance.x1 = x1
+		instance.y1 = y1
+		instance.width = width
+		instance.height = height
+		return instance
+
+	@staticmethod
+	def from_coordinates(x1, y1, x2, y2):
+		width = x2 - x1
+		height = y2 - y1
+		return BoundingBox.from_size(x1, y1, width, height)
+
+	def relative_to(self, other):
+		return BoundingBox.from_size(self.x1 - other.x1, self.y1 - other.y1, self.width, self.height)
+
+
 class Page(object):
 	blocks = []
+	size = None
 
 
 class Block(object):
@@ -67,6 +110,7 @@ class Analyzer(object):
 		self.api.Recognize()
 		iterator = self.api.GetIterator()
 		page.blocks = self.__decode_blocks(iterator, image)
+		page.size = Size(*image.size)
 
 		return page
 
@@ -77,8 +121,8 @@ class Analyzer(object):
 		blocks = []
 		for tesseract_block in iterate_level(iterator, RIL.BLOCK):
 			block = Block()
-			block.bounding_box = tesseract_block.BoundingBox(RIL.BLOCK)
-			if not tesseract_block.GetUTF8Text(RIL.BLOCK):
+			block.bounding_box = BoundingBox.from_coordinates(*tesseract_block.BoundingBox(RIL.BLOCK))
+			if not tesseract_block.GetUTF8Text(RIL.BLOCK).strip():
 				block.image = tesseract_block.GetImage(RIL.BLOCK, 0, image)
 				blocks.append(block)
 				continue
@@ -90,7 +134,7 @@ class Analyzer(object):
 		paragraphs = []
 		for tesseract_paragraph in iterate_level(iterator, RIL.PARA):
 			paragraph = Paragraph()
-			paragraph.bounding_box = tesseract_paragraph.BoundingBox(RIL.PARA)
+			paragraph.bounding_box = BoundingBox.from_coordinates(*tesseract_paragraph.BoundingBox(RIL.PARA))
 			paragraph.lines = self.__decode_lines(iterator)
 			paragraphs.append(paragraph)
 			if iterator.IsAtFinalElement(RIL.BLOCK, RIL.PARA):
@@ -101,7 +145,7 @@ class Analyzer(object):
 		lines = []
 		for tesseract_line in iterate_level(iterator, RIL.TEXTLINE):
 			line = TextLine()
-			line.bounding_box = tesseract_line.BoundingBox(RIL.TEXTLINE)
+			line.bounding_box = BoundingBox.from_coordinates(*tesseract_line.BoundingBox(RIL.TEXTLINE))
 			line.words = self.__decode_words(iterator)
 			lines.append(line)
 			if iterator.IsAtFinalElement(RIL.PARA, RIL.TEXTLINE):
@@ -113,7 +157,7 @@ class Analyzer(object):
 		for tesseract_word in iterate_level(iterator, RIL.WORD):
 			font_attributes = tesseract_word.WordFontAttributes()
 			word = Word()
-			word.bounding_box = tesseract_word.BoundingBox(RIL.WORD)
+			word.bounding_box = BoundingBox.from_coordinates(*tesseract_word.BoundingBox(RIL.WORD))
 			word.confidence = float(tesseract_word.Confidence(RIL.WORD)) / 100.0
 			word.symbols = self.__decode_symbols(iterator)
 			word.font.bold = font_attributes['bold']
@@ -132,7 +176,7 @@ class Analyzer(object):
 		symbols = []
 		for tesseract_symbol in iterate_level(iterator, RIL.SYMBOL):
 			symbol = Symbol()
-			symbol.bounding_box = tesseract_symbol.BoundingBox(RIL.SYMBOL)
+			symbol.bounding_box = BoundingBox.from_coordinates(*tesseract_symbol.BoundingBox(RIL.SYMBOL))
 			symbol.confidence = float(tesseract_symbol.Confidence(RIL.SYMBOL)) / 100.0
 			symbol.text = tesseract_symbol.GetUTF8Text(RIL.SYMBOL)
 			symbol.image = tesseract_symbol.GetBinaryImage(RIL.SYMBOL)
--- scan2ocrpdf/debug.py
+++ scan2ocrpdf/debug.py
@@ -1,37 +1,52 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from collections import namedtuple
 import os
 
 from .jinja2 import env
 from .utils import makedirs
 
 
+AnalyzePageRequest = namedtuple('AnalyzePageRequest', ['page', 'image', 'image_path'])
+
+
+def write_template(template_name, output_name, dest_dir, context):
+	template = env.get_template(template_name)
+	result = template.render(**context) # pylint: disable=no-member
+	with open(os.path.join(dest_dir, output_name), 'w') as f:
+		f.write(result)
+
+
 class AnalyzedPageDebugGenerator(object):
 	def __init__(self, debug_dir):
 		self.debug_dir = debug_dir
+		self.request = None
 		self.__dest_dir = None
 
 	def generate(self, page, image, image_path):
+		self.request = AnalyzePageRequest(page, image, image_path)
 		if not self.__debugging_enabled():
 			return
-		self.__make_dest_dir(image_path)
-		self.__generate_html_output(page, image)
+		self.__make_dest_dir()
+		self.__generate_html_output()
 
 	def __debugging_enabled(self):
 		return self.debug_dir is not None
 
-	def __make_dest_dir(self, image_path):
-		self.__dest_dir = os.path.join(self.debug_dir, os.path.basename(image_path))
+	def __make_dest_dir(self):
+		self.__dest_dir = os.path.join(self.debug_dir, os.path.basename(self.request.image_path))
 		makedirs(self.__dest_dir)
 		makedirs(os.path.join(self.__dest_dir, 'images'))
+		makedirs(os.path.join(self.__dest_dir, 'css'))
 
-	def __generate_html_output(self, page, image):
-		print(page, image)
-		template = env.get_template('analyzed_page_debug.html')
-		result = template.render(**self.__get_template_context()) # pylint: disable=no-member
-		with open(os.path.join(self.__dest_dir, 'page.html'), 'w') as f:
-			f.write(result)
+	def __generate_html_output(self):
+		context = self.__get_template_context()
+		write_template('analyzed_page_debug.html', 'page.html', self.__dest_dir, context)
+		write_template('analyzed_page_debug.css', 'page.css', os.path.join(self.__dest_dir, 'css'), context)
 
 	def __get_template_context(self):
-		return {}
+		return {
+			'title': os.path.basename(self.request.image_path),
+			'page': self.request.page,
+		}
